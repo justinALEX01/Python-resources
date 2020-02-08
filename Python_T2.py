ca9b6c3a47e5a00c3ee5754a914e4c9fe8bd4f4c
justinALEX01 <justinalexandru_96@yahoo.com>
2020-02-08 10:28:57 +0200

    Update and rename Python_T2.1.py to Python_T2.py

--- Python_T2.py
+++ Python_T2.py
@@ -34,8 +34,6 @@ def EvenOddSum(a, n):
     print("Even index positions sum ", even)
     print("nOdd index positions sum ", odd)
 
-x = [1, 2, 3, 4, 5, 5, 4, 3, 2, 1]
-
 n = len(x)
 
 EvenOddSum(x, n)
@@ -54,3 +52,33 @@ res = str(x) == str(x)[::-1]
 
 # printing result
 print("Is the number palindrome ? : " + str(res))
+
+##Second Part
+x = [5, 4, 3, 2, 0, 2]
+print(max(x))
+
+sum_ = 0
+for item in x:
+    if item % 2 != 0:
+        sum_ += item
+print(sum_)
+
+def getMaxLength(arr, n):
+    count = 0
+    result = 0
+
+    for i in range(0, n):
+        if (arr[i] == 0):
+            count = 0
+        else:
+            count += 1
+            result = max(result, count)
+
+    return result
+num = [5,4,1,2,3,0]
+
+for i in range(len(num)-1):
+    if num[i] + 1 == num[i + 1]:
+        if i == 0 or (i - 1 >= 0 and num[i - 1] != num[i] - 1):
+            print('Consecutive', num[i])
+        print('Consecutive', num[i + 1])
